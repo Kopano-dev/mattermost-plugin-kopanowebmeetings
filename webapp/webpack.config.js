051c47a8d964611bb46dbd30a5d82f7e6c9a51b4
Simon Eisenmann <simon@longsleep.org>
2017-12-11 18:26:35 +0100

    Reorganize Makefile
    This change updates the project to use yarn instead of npm by updating
    the Makefiles and splitting the Makefile to prepare for integration of a
    server side plugin.

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './index.js',
     output: {
-        filename: 'dist/kopanowebmeetings_bundle.js'
+        filename: 'build/kopanowebmeetings_bundle.js'
     },
     module: {
         rules: [
